3a17263ef0a3d1674a4835ce93c565f4f965db87
Dowdow <leo.riera1@gmail.com>
2014-05-27 10:40:13 +0200

    Ajout de l édition et de la suppression de list

--- src/LeoAndLeo/ToDoListBundle/Controller/ListController.php
+++ src/LeoAndLeo/ToDoListBundle/Controller/ListController.php
@@ -9,13 +9,11 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ListController extends Controller
 {
-    public function indexAction()
-    {
+    public function indexAction() {
         return $this->render('LeoAndLeoToDoListBundle:Page:index.html.twig');
     }
 
-    public function listAction()
-    {
+    public function listAction() {
         $em = $this->getDoctrine()->getManager();
         $listRepo = $em->getRepository('LeoAndLeoToDoListBundle:MainList');
 
@@ -24,8 +22,19 @@ class ListController extends Controller
         return $this->render('LeoAndLeoToDoListBundle:Page:list.html.twig', array('lists' => $lists));
     }
 
-    public function listAddAction()
-    {
+    public function listIdAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $listRepo = $em->getRepository('LeoAndLeoToDoListBundle:MainList');
+
+        $list = $listRepo->findOneById($id);
+        if($list == null) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('LeoAndLeoToDoListBundle:Page:listid.html.twig', array('list' => $list));
+    }
+
+    public function listAddAction() {
         $em = $this->getDoctrine()->getManager();
 
         // Creation of the form
@@ -46,16 +55,40 @@ class ListController extends Controller
         return $this->render('LeoAndLeoToDoListBundle:Page:listadd.html.twig', array('form' => $form->createView()));
     }
 
-    public function listIdAction($id)
-    {
+    public function listEditAction($id) {
         $em = $this->getDoctrine()->getManager();
-        $listRepo = $em->getRepository('LeoAndLeoToDoListBundle:MainList');
+        $listrepo = $em->getRepository('LeoAndLeoToDoListBundle:MainList');
 
-        $list = $listRepo->findOneById($id);
-        if($list == null) {
+        // Creation of the form
+        $mainlist = $listrepo->findOneById($id);
+        if($mainlist == null) {
             throw new NotFoundHttpException();
         }
+        $form = $this->createForm(new MainListType(), $mainlist);
 
-        return $this->render('LeoAndLeoToDoListBundle:Page:listid.html.twig', array('list' => $list));
+        // Exploitation of the form
+        $request = $this->get('request');
+        if($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            if($form->isValid()){
+                $em->flush();
+                return $this->redirect($this->generateUrl('leo_and_leo_to_do_list.list_list_id', array('id' => $mainlist->getId())));
+            }
+        }
+
+        return $this->render('LeoAndLeoToDoListBundle:Page:listedit.html.twig', array('form' => $form->createView()));
+    }
+
+    public function listRemoveAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $listrepo = $em->getRepository('LeoAndLeoToDoListBundle:MainList');
+
+        $mainlist = $listrepo->findOneById($id);
+        if($mainlist == null) {
+            throw new NotFoundHttpException();
+        }
+        $em->remove($mainlist);
+        $em->flush();
+        return $this->render('LeoAndLeoToDoListBundle:Page:listremove.html.twig');
     }
 }
